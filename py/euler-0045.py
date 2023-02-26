c6c0e4c39a0a4a35f8bb9280210421ee9f9801e5
John Driscoll <john@bitgo.com>
2023-02-26 12:43:28 -0600

    Add solution for 45

---
+++ py/euler-0045.py
@@ -0,0 +1,19 @@
+def is_pentagonal(n):
+  return (1 + (1 + 24 * n) ** (1 / 2)) % 6 == 0
+
+def is_triangular(n):
+  return ((1 + 8 * n) ** (1 / 2) - 1) % 2 == 0
+
+def is_hexagonal(n):
+  return (1 + (1 + 8 * n) ** (1 / 2)) % 4 == 0
+
+def triangular(n):
+  return (n * (n + 1)) // 2
+
+n = 286
+while True:
+  t = triangular(n)
+  if is_pentagonal(t) and is_hexagonal(t):
+    print(t)
+    exit()
+  n += 1
