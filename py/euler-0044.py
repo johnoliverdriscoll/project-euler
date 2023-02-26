913990f2daa28fe1a1fd410dc9e4d2cd3fd8156b
John Driscoll <john@bitgo.com>
2023-02-26 12:30:38 -0600

    Add solution for 44

---
+++ py/euler-0044.py
@@ -0,0 +1,13 @@
+def is_pentagonal(n):
+  return (1 + (1 + 24 * n) ** (1 / 2)) % 6 == 0
+
+def pentagonal(n):
+  return (n * (3 * n - 1)) // 2
+
+i = 1
+while True:
+  for j in reversed(range(1, i + 1)):
+    if is_pentagonal(pentagonal(i) + pentagonal(j)) and is_pentagonal(pentagonal(i) - pentagonal(j)):
+      print(pentagonal(i) - pentagonal(j))
+      exit()
+  i += 1
