7f2247e9b0fe51a7841109ca6b40085d64adaedd
John Driscoll <john@bitgo.com>
2023-02-27 09:59:47 -0600

    Add solution for 50

---
+++ py/euler-0050.py
@@ -0,0 +1,44 @@
+import sys
+
+def is_prime(n, p = dict()):
+  if n in p:
+    return p[n]
+  if n < 2:
+    return False
+  for x in range(2, int(n ** (1 / 2)) + 1):
+    if n % x == 0:
+      p[n] = False
+      break
+  if not n in p:
+    p[n] = True
+  return p[n]
+
+def prime():
+  yield 2
+  n = 3
+  while True:
+    if is_prime(n):
+      yield n
+    n += 2
+
+n = int(sys.argv[1])
+p = []
+it = prime()
+m = 0
+c = 0
+while True:
+  p.append(next(it))
+  if sum(p[-c:]) > n:
+    break
+  i, j, l = 0, 0, 0
+  while i < len(p):
+    s = sum(p[i:])
+    i += 1
+    if s < n and is_prime(s):
+      j = len(p) - i
+      l = s
+      break
+  if c < j:
+    m = l
+    c = j
+print(m)
