8f98a95cc0cabd66e6de21901848e06e0c10181d
John Driscoll <john@bitgo.com>
2023-03-04 22:05:20 -0600

    Add solution for 57

---
+++ py/euler-0057.py
@@ -0,0 +1,17 @@
+from math import log10
+
+def sqrt2():
+  n = 3
+  d = 2
+  yield (n, d)
+  while True:
+    n, d = n + 2 * d, n + d
+    yield (n, d)
+
+it = sqrt2()
+c = 0
+for i in range(0, 1000):
+  n, d = next(it)
+  if int(log10(n)) > int(log10(d)):
+    c += 1
+print(c)
