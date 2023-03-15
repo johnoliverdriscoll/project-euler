1cb3f249ab4647d8766162584ced9e50e75be266
John Driscoll <john@bitgo.com>
2023-03-14 21:11:24 -0500

    Add solution for 65

---
+++ py/euler-0065.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+def denominator(i):
+  if i == 1:
+    return 2
+  if i % 3 == 0:
+    return 2 * i // 3
+  return 1
+
+r = int(argv[1])
+n = 1
+d = denominator(r)
+for i in reversed(range(1, r)):
+  n += d * denominator(i)
+  n, d = d, n
+print(sum(map(int, str(d))))
