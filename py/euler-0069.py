f282d9b853cf838b95c25f6b0c1e8095086900e4
John Driscoll <johnoliverdriscoll@gmail.com>
2023-03-20 01:36:48 -0500

    Add solution for 69

---
+++ py/euler-0069.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+def check_primality_for_n_gt_2(n):
+  for x in range(2, int(n ** (1 / 2)) + 1):
+    if n % x == 0:
+      return False
+  return True
+
+def is_prime(n, memo=dict()):
+  if n < 2:
+    return False
+  if n == 2:
+    return True
+  if n not in memo:
+    memo[n] = check_primality_for_n_gt_2(n)
+  return memo[n]
+
+def get_prime():
+  yield 2
+  p = 3
+  while 1:
+    if is_prime(p):
+      yield p
+    p += 2
+
+x = 1
+for p in get_prime():
+  if x * p > int(argv[1]):
+    print(x)
+    exit()
+  else:
+    x *= p
