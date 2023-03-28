d325e5639e8bc20312b236dc69a14550b7d03e7f
John Driscoll <johnoliverdriscoll@gmail.com>
2023-03-28 16:03:10 -0500

    Add solution for 79

---
+++ py/euler-0079.py
@@ -0,0 +1,27 @@
+from os import path
+
+f = open(path.join(path.dirname(__file__), '../resources/p079_keylog.txt'))
+keylog = []
+while 1:
+  l = f.readline()
+  if not l:
+    break
+  keylog.append([int(c) for c in l.strip()])
+weights = dict()
+for key in keylog:
+  d = weights.get(key[1], (set(), set()))
+  d[0].add(key[0])
+  d[1].add(key[2])
+  weights[key[1]] = d
+digits = list(weights.keys())
+start = [x for x in weights if len(weights[x][0]) == 1][0]
+end = [x for x in weights if len(weights[x][1]) == 1][0]
+key = [list(weights[start][0])[0], start]
+curr = start
+while curr != end:
+  w = [d for d in digits if d != curr]
+  left = [len(weights[d][0] ^ set(key)) for d in w]
+  curr = w[left.index(min(left))]
+  key.append(curr)
+key += weights[end][1]
+print(''.join([str(d) for d in key]))
