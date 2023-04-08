df27288ddca32c9586a2f7793a4a00348a96845e
John Driscoll <johnoliverdriscoll@gmail.com>
2023-04-08 00:53:17 -0500

    Add solution for 87

---
+++ py/euler-0087.py
@@ -0,0 +1,52 @@
+from sys import argv
+
+def check_primality_for_n_gt_2(n):
+  for x in range(2, int(n ** (1 / 2)) + 1):
+    if n % x == 0:
+      return False
+  return True
+
+def is_prime(n, memo=dict()):
+  if n < 2:
+    return False
+  if n == 2:
+    return True
+  if n not in memo:
+    memo[n] = check_primality_for_n_gt_2(n)
+  return memo[n]
+
+def get_prime():
+  yield 2
+  p = 3
+  while 1:
+    if is_prime(p):
+      yield p
+    p += 2
+
+n = int(argv[1])
+prime_power_triples = set()
+i = get_prime()
+while 1:
+  x = next(i)
+  s = x ** 2
+  if s >= n:
+    break
+  j = get_prime()
+  while 1:
+    y = next(j)
+    s += y ** 3
+    if s >= n:
+      s -= y ** 3
+      break
+    k = get_prime()
+    while 1:
+      z = next(k)
+      s += z ** 4
+      if s >= n:
+        s -= z ** 4
+        break
+      prime_power_triples.add(s)
+      s -= z ** 4
+    s -= y ** 3
+
+print(len(prime_power_triples))
