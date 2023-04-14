339f3750118d259cb849b9fd718e48501e6161a2
John Driscoll <johnoliverdriscoll@gmail.com>
2023-04-14 05:03:42 -0500

    Optimize factorization code

--- py/euler-0012.py
+++ py/euler-0012.py
@@ -1,23 +1,27 @@
 from sys import argv
 
-def factorize(n, f = None):
-  if not f:
+def factorize(n, memo=dict()):
+  if n < 2:
+    return set()
+  f = memo.get(n, None)
+  if f == None:
     f = set()
-  f.add(1)
-  f.add(n)
-  x = 2
-  l = n // 2
-  sqrt_n = int(n ** (1 / 2)) + 1
-  while x <= l:
-    if x > sqrt_n and len(f) == 2:
-      return f
-    if n % x == 0:
-      l = n // x
-      f.add(x)
-      if not n // x in f:
-        for y in factorize(n // x, f):
-          f.add(y)
-    x += 1
+    f.add(1)
+    f.add(n)
+    x = 2
+    l = n // 2
+    sqrt_n = int(n ** (1 / 2)) + 1
+    while x <= l:
+      if x > sqrt_n and len(f) == 2:
+        return f
+      if n % x == 0:
+        l = n // x
+        f.add(x)
+        if not n // x in f:
+          for y in factorize(n // x):
+            f.add(y)
+      x += 1
+    memo[n] = f
   return f
 
 n = int(argv[1])
--- py/euler-0021.py
+++ py/euler-0021.py
@@ -1,21 +1,25 @@
-def factorize(n, f = None):
-  if not f:
+def factorize(n, memo=dict()):
+  if n < 2:
+    return set()
+  f = memo.get(n, None)
+  if f == None:
     f = set()
-  f.add(1)
-  f.add(n)
-  x = 2
-  l = n // 2
-  sqrt_n = int(n ** (1 / 2)) + 1
-  while x <= l:
-    if x > sqrt_n and len(f) == 2:
-      return f
-    if n % x == 0:
-      l = n // x
-      f.add(x)
-      if not n // x in f:
-        for y in factorize(n // x, f):
-          f.add(y)
-    x += 1
+    f.add(1)
+    f.add(n)
+    x = 2
+    l = n // 2
+    sqrt_n = int(n ** (1 / 2)) + 1
+    while x <= l:
+      if x > sqrt_n and len(f) == 2:
+        return f
+      if n % x == 0:
+        l = n // x
+        f.add(x)
+        if not n // x in f:
+          for y in factorize(n // x):
+            f.add(y)
+      x += 1
+    memo[n] = f
   return f
 
 def proper_divisors(n):
