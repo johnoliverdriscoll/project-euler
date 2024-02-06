97e589b5a35a78b620e01b22c68ec5765bf0861d
John Driscoll <john@bitgo.com>
2024-02-06 13:48:31 -0600

    Revise solution for 68

--- py/euler-0068.py
+++ py/euler-0068.py
@@ -5,13 +5,13 @@ n = int(argv[1])
 # The solution requires that the minimum external node be as large as possible.
 # We define the sequence of external nodes in descending order:
 
-E = [1 + 2 * n - i for i in range(1, n + 1)]
+E = [2 * n - i for i in range(n)]
 
 # We then define the sequence of inner nodes starting with 1 between the pair of
 # least edges, subtracting the previous node from the current edge for each
 # subsequent index as we circle the ring clockwise:
 
-I = [(n + i) // 2 if i % 2 else i // 2 for i in range(2, n + 2)]
+I = [(2 + n + i) // 2 if i % 2 else (2 + i) // 2 for i in range(n)]
 
 # In order to produce a solution set that starts with the minimal external node,
 # we start building from the maximal edge by shifting `E` and `I` to the right:
